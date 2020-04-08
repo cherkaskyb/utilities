0f7e82798b7d8fbb03b6a6a9bc74070b1f53dc2e
cherkaskyb <cherkaskyb@gmail.com>
2020-04-08 12:18:23 +0300

    Update kc-pod-bash.sh

--- kc-pod-bash.sh
+++ kc-pod-bash.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-FIRST_POD=$(kubectl get pods | grep lighthouse | head -1 | awk '{print $1}')
+POD_NAME=$1
+FIRST_POD=$(kubectl get pods | grep $POD_NAME | head -1 | awk '{print $1}')
 echo "--> connecting to $FIRST_POD <--"
 kubectl exec -it $FIRST_POD /bin/bash
